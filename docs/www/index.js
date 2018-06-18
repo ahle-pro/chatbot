a76fffe87c2f1a49137f9f5727c8b7b2e66a47f7
ahle <hisokaxmagician@gmail.com>
2018-06-18 12:57:30 +0200

    es5

--- docs/www/index.js
+++ docs/www/index.js
@@ -32,8 +32,21 @@ var current = { back: {}, stack: [] };
 start();
 
 function start() {
-    var params = new URL(document.location).searchParams;
-    var mode = params.get("mode");
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+    
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+    
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+    var mode = getUrlParameter("mode");
 
     if (mode == "init") {
         init();
